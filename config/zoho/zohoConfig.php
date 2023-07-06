e00c998f637422f148b332e4c84bcd07b7c140bc
witchblvd <eboshimosaha@gmail.com>
2023-07-06 11:15:07 +0300

    comm

---
+++ config/zoho/zohoConfig.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'client_id'=>'1000.PQHV3XO8HK1Z0PNM3OXA8P9NSFX1UE',
+    'client_secret'=>'92285ac6f1004263b8bf5e9140824b00b859181c54',
+    'refresh_token'=>'1000.b6f0974f64f0e990dcbb44fc9101a999.fb8ce5616e1432f083933968e4b15489',
+    'deluge_api_url' => 'https://www.zohoapis.com/crm/v2/functions/',
+    'zapi_key' => '1003.cfc2b77c73361da1e4f798a60cf0dace.6ea6a6adc74b7210d1a7afe47cb737fc'
+];
